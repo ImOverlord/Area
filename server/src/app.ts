50b210cf9eb626bcc45140aa6598b2c3aabb4bfa
ImOverlord <ImOverlord@hotmail.com>
2020-02-27 12:07:57 +0100

    Updated App to init Reactions

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,7 @@ import { IAction } from './Interface/IAction';
 import { Firebase } from './Modules/Firebase/Firebase';
 import { InfoModule } from './Modules/Info/Info';
 import { Monitor } from './Modules/Monitor/Monitor';
+import { IReaction } from './Interface/IReaction';
 
 /** This is the entrypoint */
 
@@ -16,12 +17,15 @@ Promise.all([
     loadFiles("Actions/");
     loadFiles("Reactions/");
     const actions = inject.getByValue<IAction>('type', 'action');
+    const reactions = inject.getByValue<IReaction>('type', 'reaction');
     const promises = [
         inject.inject(InfoModule).init(),
         inject.inject(Monitor).init()
     ];
     for (const action of actions)
         promises.push(action.init());
+    for (const reaction of reactions)
+        promises.push(reaction.init());
     return Promise.all(promises);
 })
 .then(() => {
