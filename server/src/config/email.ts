c14f76cd6d597db127b18487c8d04cedc21ecd7a
Hugo Courthias <hugo.courthias@gmail.com>
2020-03-01 16:42:09 +0100

    Added server files

---
+++ server/src/config/email.ts
@@ -0,0 +1,9 @@
+export const mailConfig = {
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'area2020epi@gmail.com',
+        pass: 'Epitech2020'
+    }
+};
