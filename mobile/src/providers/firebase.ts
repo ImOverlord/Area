3fb635420642a328c891e005257e6a1de62bb9b3
Hugo Courthias <hugo.courthias@gmail.com>
2020-03-01 16:50:41 +0100

    Fix merge conflicts

--- mobile/src/providers/firebase.ts
+++ mobile/src/providers/firebase.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-import firebase from "firebase";
->>>>>>> edge
 import {
   API_KEY,
   APP_ID,
@@ -11,13 +7,10 @@ import {
   PROJECT_ID
 } from "react-native-dotenv";
 
-<<<<<<< HEAD
 const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
 
-=======
->>>>>>> edge
 const firebaseConfig = {
   apiKey: API_KEY,
   authDomain: AUTH_DOMAIN,
@@ -29,13 +22,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-<<<<<<< HEAD
 const Firebase = firebase.initializeApp(firebaseConfig) as firebase.app.App;
 export const db = firebase.firestore() as firebase.firestore.Firestore;
 
-=======
-const Firebase = firebase.initializeApp(firebaseConfig);
->>>>>>> edge
 export const GOOGLE_CLIENT_IOS_STAND =
   "367765098795-oj72gj0p2a9iel8bf7ruajesgsaclvdr.apps.googleusercontent.com";
 export const WEB_CLIENT_ID =
--- mobile/src/screens/Home/styles.ts
+++ mobile/src/screens/Home/styles.ts
@@ -2,10 +2,7 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   topContainer: {
-<<<<<<< HEAD
     backgroundColor: "white",
-=======
->>>>>>> edge
     padding: 24,
     flexDirection: "row",
     justifyContent: "space-between",
@@ -18,23 +15,12 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   title: {
-<<<<<<< HEAD
     fontFamily: "AvenirNext-Heavy",
     fontSize: 42
   },
   email: {
     fontFamily: "AvenirNext-Bold",
     fontSize: 14
-=======
-    fontFamily: "Avenir Next",
-    fontSize: 42,
-    fontWeight: "900"
-  },
-  email: {
-    fontFamily: "Avenir Next",
-    fontSize: 14,
-    fontWeight: "700"
->>>>>>> edge
   },
   bottomButton: {
     paddingVertical: 8,
@@ -43,23 +29,10 @@ const styles = StyleSheet.create({
     borderRadius: 25
   },
   bottomButtonText: {
-<<<<<<< HEAD
     fontFamily: "AvenirNext-Bold",
     fontSize: 18,
     color: "white"
   },
-=======
-    fontFamily: "Avenir Next",
-    fontSize: 18,
-    fontWeight: "700",
-    color: "white"
-  },
-  bsWrapper: {
-    padding: 16,
-    backgroundColor: "white",
-    height: "100%"
-  },
->>>>>>> edge
   topSafeAreaView: { flex: 0, backgroundColor: "white" },
   bottomSafeAreaView: { flex: 1, backgroundColor: "black" },
   scrollView: { flex: 1, backgroundColor: "white" }
